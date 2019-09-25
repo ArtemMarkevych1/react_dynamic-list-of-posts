e8c740b757661123ea513f2d4d8fe84c9d472fbb
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-09-25 15:17:43 +0300

    Updates

---
+++ precache-manifest.1023ce4c7a2849f08804afaf79bceb67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd92b27d284ea0fa10119958153129e1",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "dbf13c62383798423edf",
+    "url": "/react_dynamic-list-of-posts/static/css/main.3573db65.chunk.css"
+  },
+  {
+    "revision": "458e97df46596ee9ca41",
+    "url": "/react_dynamic-list-of-posts/static/js/2.a3e16a20.chunk.js"
+  },
+  {
+    "revision": "dbf13c62383798423edf",
+    "url": "/react_dynamic-list-of-posts/static/js/main.7e201cfb.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
